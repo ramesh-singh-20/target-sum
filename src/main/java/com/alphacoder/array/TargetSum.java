31081c31d07b1de7348becef8069b68fc7c4dc1f
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-11-11 16:56:00 -0500

    Added 2 approaches.

--- src/main/java/com/alphacoder/array/TargetSum.java
+++ src/main/java/com/alphacoder/array/TargetSum.java
@@ -1,4 +1,54 @@
 package com.alphacoder.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TargetSum {
+
+    public static List<Integer> returnIndicesWithTwoForLoops(int [] arr, int target){
+        List<Integer> indices= new ArrayList<>();
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                    indices.add(i);
+                    indices.add(j);
+                    break;
+                }
+            }
+        }
+
+        if(indices.isEmpty()){
+            throw new RuntimeException("Target sum does not exists.");
+        }
+
+        return indices;
+    }
+
+    public static List<Integer> returnIndicesWithSorting(int [] arr, int target){
+        List<Integer> indices= new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        for(int i=0, j= arr.length-1; i<=(i+j)/2 && j>=(i+j)/2;){
+            if(arr[i]+arr[j]== target){
+                indices.add(i);
+                indices.add(j);
+                break;
+            }
+            else if(arr[i]+arr[j]< target){
+                i++;
+            }
+            else if(arr[i]+arr[j]> target){
+                j--;
+            }
+        }
+
+        if(indices.isEmpty()){
+            throw new RuntimeException("Target sum does not exists.");
+        }
+
+        return indices;
+    }
 }
